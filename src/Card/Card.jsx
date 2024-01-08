5ad2880b52dca7f1de78f8c7f0fa3d63528ff4ff
Digontha <digonthodj@gmail.com>
2024-01-08 11:08:45 +0600

    create modal

--- src/Card/Card.jsx
+++ src/Card/Card.jsx
@@ -36,6 +36,12 @@ const Card = () => {
                 <section className="mt-[48px] flex gap-[340px]">
 
                     <div className="mx-auto">
+                        <div className="flex justify-between items-center mb-5 px-2 lg:hidden">
+                            <p className="font-bold">Posts(4)</p>
+                            <div>
+                                <p className="flex items-center gap-2 bg-[#EDEEF0] text-[15px] font-medium py-[8px] px-[10px] rounded-[4px]">Filter:All <IoMdArrowDropdown></IoMdArrowDropdown></p>
+                            </div>
+                        </div>
                         <div className="lg:w-[692px] w-[370px] lg:h-[220px] h-[120px] mx-auto lg:mx-0 border shadow-md rounded-t">
                             <div>
                                 <figure>
@@ -107,7 +113,7 @@ const Card = () => {
 
                                 <div className="flex justify-between items-center">
                                     <div className="lg:w-[600px] w-[300px] font-semibold">
-                                        <p className="lg:text-[22px] text-[18px]">What if famous brands had regular fonts? Meet RegulaBrands!</p>
+                                        <p className="lg:text-[22px] text-[18px]">Iconic brands get a down-to-earth makeover in RegulaBrands</p>
                                     </div>
                                     <div className="dropdown dropdown-bottom dropdown-end">
                                         <div tabIndex={0} role="button" className="text-xl m-1"><BsThreeDots></BsThreeDots></div>
@@ -120,14 +126,14 @@ const Card = () => {
                                 </div>
 
                                 <div>
-                                    <p className="lg:text-[19px] text-[15px] text-[#5C5C5C] font-normal">I’ve worked in UX for the better part of a decade. From now on, I plan to rei…</p>
+                                    <p className="lg:text-[19px] text-[15px] text-[#5C5C5C] font-normal">where the extraordinary meets the simplicity of regular fonts…</p>
                                 </div>
 
                                 <div className="lg:px-5 mt-6 py-8 flex justify-between items-center">
                                     <div className="flex items-center gap-3">
 
                                         <div>
-                                            <img src="https://img.freepik.com/free-photo/young-bearded-man-with-striped-shirt_273609-5677.jpg" className="w-10 h-10 rounded-full" alt="" />
+                                            <img src="https://th.bing.com/th/id/R.8af0b5f248739652d2f25a273f164d90?rik=SVvf1eEeQBvIiw&pid=ImgRaw&r=0" className="w-10 h-10 rounded-full" alt="" />
                                         </div>
                                         <p>Sarthak Kamra</p>
 
@@ -164,7 +170,7 @@ const Card = () => {
 
                                 <div className="flex justify-between items-center">
                                     <div className="lg:w-[600px] w-[300px] font-semibold">
-                                        <p className="lg:text-[22px] text-[18px]">What if famous brands had regular fonts? Meet RegulaBrands!</p>
+                                        <p className="lg:text-[22px] text-[18px]">Step into the world of RegulaBrands? Meet RegulaBrands!</p>
                                     </div>
                                     <div className="dropdown dropdown-bottom dropdown-end">
                                         <div tabIndex={0} role="button" className="text-xl m-1"><BsThreeDots></BsThreeDots></div>
@@ -184,7 +190,7 @@ const Card = () => {
                                     <div className="flex items-center gap-3">
 
                                         <div>
-                                            <img src="https://img.freepik.com/free-photo/young-bearded-man-with-striped-shirt_273609-5677.jpg" className="w-10 h-10 rounded-full" alt="" />
+                                            <img src="https://thumbs.dreamstime.com/b/man-12466775.jpg" className="w-10 h-10 rounded-full" alt="" />
                                         </div>
                                         <p>Sarthak Kamra</p>
 
